d577259ef1c9b56c99c18290394e3b839fb1072a
Niklas Anderson <nkanders@gmail.com>
2026-06-06 15:11:21 -0700

    Add menu screen functionality
    Adds start and end screens, and displays the score during gameplay.

--- NoteFeller-App/src/main.c
+++ NoteFeller-App/src/main.c
@@ -22,6 +22,7 @@
 #include "vga_sprite.h"
 #include "note.h"
 #include "key.h"
+#include "menu.h"
 
 typedef enum {
     GAME_STATE_START = 0,
@@ -173,10 +174,12 @@ static void system_init(void)
     note_init_notes();
 
     game_state = GAME_STATE_START;
+    menu_start_screen();
 }
 
 static void reset_gameplay(void)
 {
+    vga_clear_all_sprites();
     score_reset();
     audio_silence();
     input_clear_all_edges();
@@ -225,8 +228,11 @@ static void playing_update(void)
 
     process_note_hits(presses);
 
+    menu_draw_score();
+
     if (presses & INPUT_LANE_4) {
         audio_silence();
+        menu_end_screen();
         game_state = GAME_STATE_END;
     }
 }
@@ -238,7 +244,8 @@ static void end_screen_update(void)
     uint32_t presses = input_poll_new_presses();
 
     if (presses & INPUT_LANE_4) {
-        game_state = GAME_STATE_START;
+        reset_gameplay();
+        game_state = GAME_STATE_PLAYING;
     }
 }
 
